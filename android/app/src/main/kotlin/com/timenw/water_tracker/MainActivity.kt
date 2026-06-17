e6d4f20c0d73cb1fe4d4a6083f172c7b4ae8e89a
root <root@nQb9PPVA7v.flapjacks.com>
2026-06-17 01:15:04 +0000

    chore: remove manual Android files, let flutter create generate them in CI

--- android/app/src/main/kotlin/com/timenw/water_tracker/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-// android/app/src/main/kotlin/com/timenw/water_tracker/MainActivity.kt
-package com.timenw.water_tracker
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
