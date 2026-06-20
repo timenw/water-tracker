728b62d692927d6aa7b5a24fb82d4a08d711b656
root <root@nQb9PPVA7v.flapjacks.com>
2026-06-20 22:54:02 +0000

    fix: add missing dp import in WaterTab.kt

--- android/app/src/main/java/com/timenw/watertracker/ui/screens/WaterTab.kt
+++ android/app/src/main/java/com/timenw/watertracker/ui/screens/WaterTab.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.timenw.watertracker.data.model.DailyWaterGoal
 import com.timenw.watertracker.data.model.WaterRecord
